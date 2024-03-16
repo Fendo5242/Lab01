18fb7ac0b30788e0e1470bc8e4afb390eb57b1b1
Fernando Sanchez Berru <91902120+Fendo5242@users.noreply.github.com>
2024-03-16 12:45:06 -0500

    Add files via upload

---
+++ Lab01/CuentaBancaria.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    public class CuentaBancaria
+    {
+        public string NumeroCuenta { get; set; }
+        public string TitularCuenta { get; set; }
+        public int Saldo { get; set; } = 1000;
+        public string PIN { get; set; }
+        public int Fondo { get; set; } 
+        public int Retiro { get; set; }
+
+        public string nuevopin {  get; set; }
+
+        public class CajeroAutomatico : CuentaBancaria
+        {
+            public int ConsultarSaldo()
+            {
+                Console.WriteLine("Cuentas con un saldo de: " + Saldo);
+
+                return Saldo;
+            }
+
+            public int DepositarFondo() {
+
+                Console.WriteLine("¿Cuanto desea depositar?: ");
+
+                Fondo = int.Parse(Console.ReadLine());
+
+                Saldo = Saldo + Fondo;
+
+                Console.WriteLine("Ahora cuentas con: " + Saldo);
+                return Saldo;
+            }
+
+            public int RetirarEfectivo()
+            {
+                Console.WriteLine("¿Cuanto desea retirar?: ");
+                Retiro = int.Parse(Console.ReadLine());
+
+                if (Retiro > Saldo)
+                {
+                    Console.WriteLine("No cuentas con el saldo para hacer ese retiro. Tu saldo: ");
+                    return Saldo;
+                }else
+                {
+                    Saldo = Saldo - Retiro;
+                    Console.WriteLine("Ahora cuentas con: " + Saldo);
+                    return Saldo;
+                }
+                
+            }
+
+            public string CambiarPIN()
+            {
+                Console.WriteLine("Introdusca el nuevo PIN: ");
+                nuevopin = Console.ReadLine();
+                if (nuevopin == PIN)
+                {
+                    Console.WriteLine("Debe introducir un PIN diferente al anterior.");
+                }else if (nuevopin != PIN) {
+                    PIN = nuevopin;
+                    Console.WriteLine("Cambio exitoso, su nuevo PIN es: " + PIN);
+                }
+                return PIN;
+            }
+        }
+
+    }
+}
---
+++ Lab01/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab01;
+
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Bienvenido al Cajero Automatico Sanchez, introdusca sus datos: ");
+Console.WriteLine("----------------------------------------");
+
+Console.WriteLine("Número de cuenta: ");
+
+CuentaBancaria cuenta = new CuentaBancaria();
+CuentaBancaria.CajeroAutomatico cajero = new CuentaBancaria.CajeroAutomatico();
+
+cuenta.NumeroCuenta = Console.ReadLine();
+
+Console.WriteLine("Nombre del titular: ");
+
+cuenta.TitularCuenta = Console.ReadLine();
+
+Console.WriteLine("PIN");
+
+cuenta.PIN = Console.ReadLine();
+
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Los datos son correctos");
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Su número de cuenta es: " + cuenta.NumeroCuenta);
+Console.WriteLine("Su nombre es: " + cuenta.TitularCuenta);
+Console.WriteLine("Su PIN es: " + cuenta.PIN);
+Console.WriteLine("----------------------------------------");
+int opcion = 0;
+
+while (opcion != 5)
+{
+    Console.WriteLine("Desee la acción que requiere realizar: ");
+    Console.WriteLine("(1) -> Consultar saldo");
+    Console.WriteLine("(2) -> Depositar");
+    Console.WriteLine("(3) -> Retirar");
+    Console.WriteLine("(4) -> Cambiar PIN");
+    Console.WriteLine("(5) -> Salir");
+    opcion = int.Parse(Console.ReadLine());
+
+    if (opcion == 1)
+    {
+        cajero.ConsultarSaldo();
+
+    }else if (opcion == 2)
+    {
+        cajero.DepositarFondo();
+    }
+    else if (opcion == 3)
+    {
+        cajero.RetirarEfectivo();
+    }
+    else if (opcion == 4)
+    {
+        cajero.CambiarPIN();
+    }
+
+}
+    
+
+
+
+
+
